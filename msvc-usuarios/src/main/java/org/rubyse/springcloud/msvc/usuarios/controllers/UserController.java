60e7b3d152c68679eac28c98ef11dda14384aaef
RubySE <lucanagiuliana@gmail.com>
2026-06-04 01:12:32 -0500

    Initial database configuration and user controller setup

--- msvc-usuarios/src/main/java/org/rubyse/springcloud/msvc/usuarios/controllers/UserController.java
+++ msvc-usuarios/src/main/java/org/rubyse/springcloud/msvc/usuarios/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.rubyse.springcloud.msvc.usuarios.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,4 +59,16 @@ public class UserController {
         return ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        Optional<User> o = service.findById(id);
+        if (o.isPresent()) {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        }    
+
+        return ResponseEntity.notFound().build();
+
+    }
 }
+
